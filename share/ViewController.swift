a22df5aa75c16b06e9d025ca06893b9f3f71736a
JOSEPHINE SHREEVES <josephine.shreeves19@clcmbp19.local>
2019-04-30 12:46:46 -0500

    Initial Commit

---
+++ share/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  share
+//
+//  Created by JOSEPHINE SHREEVES on 4/30/19.
+//  Copyright © 2019 clc.shreeves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var num = arc4random_uniform(10) + 1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(num)
+    
+    }
+
+func haveFun(){
+print("I hope you have fun today !!")
+}
+}
